375501da1dd09f4d309143e176b5011ec1aed974
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2020-01-20 13:45:49 +0900

    fix dataset loading

--- transformer.py
+++ transformer.py
@@ -2,7 +2,9 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchtext.data as data
 import itertools
+from tqdm import tqdm
 
 class TransformerModel(nn.Module):
     def __init__(self, ntoken, ninp, nhead, nhid, nlayers, device, dropout=0.5):
@@ -72,38 +74,21 @@ train_txt, val_txt, test_txt = torchtext.datasets.WikiText2.splits(TEXT)
 TEXT.build_vocab(train_txt)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def batchify(data, bsz):
-    data = TEXT.numericalize([data.examples[0].text])
-    # Divide the dataset into bsz parts.
-    nbatch = data.size(0) // bsz
-    # Trim off any extra elements that wouldn't cleanly fit (remainders).
-    data = data.narrow(0, 0, nbatch * bsz)
-    # Evenly divide the data across the bsz batches.
-    data = data.view(bsz, -1).t().contiguous()
-    return data.to(device)
-
 batch_size = 20
 eval_batch_size = 10
-train_data = batchify(train_txt, batch_size)
-val_data = batchify(val_txt, eval_batch_size)
-test_data = batchify(test_txt, eval_batch_size)
-
-bptt = 35
-def get_batch(source, i):
-    seq_len = min(bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len]
-    target = source[i+1:i+1+seq_len].view(-1)
-    return data, target
+bptt_len = 35
+train_iter, val_iter, test_iter = data.BPTTIterator.splits(
+        (train_txt, val_txt, test_txt), batch_sizes=(batch_size, eval_batch_size, eval_batch_size), bptt_len=bptt_len, device=device)
 
 ntokens = len(TEXT.vocab.stoi) # the size of vocabulary
 emsize = 200 # embedding dimension
-nhid = 200 # the dimension of the feedforward network model in nn.TransformerEncoder
-nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
+nhid = 80 # the dimension of the feedforward network model in nn.TransformerEncoder
+nlayers = 5 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
 #nlayers = 4 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
 nhead = 2 # the number of heads in the multiheadattention models
 #nhead = 4 # the number of heads in the multiheadattention models
-#dropout = 0.2 # the dropout value
-dropout = 0.4 # the dropout value
+dropout = 0.2 # the dropout value
+#dropout = 0.4 # the dropout value
 model = TransformerModel(ntokens, emsize, nhead, nhid, nlayers, device, dropout).to(device)
 
 criterion = nn.CrossEntropyLoss()
@@ -117,44 +102,42 @@ def train():
     total_loss = 0.
     start_time = time.time()
     ntokens = len(TEXT.vocab.stoi)
-    for batch, i in enumerate(range(0, train_data.size(0) - 1, bptt)):
-        data, targets = get_batch(train_data, i)
+    for i, batch in enumerate(train_iter):
         optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output.view(-1, ntokens), targets)
+        output = model(batch.text)
+        loss = criterion(output.view(-1, ntokens), batch.target.view(-1))
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
         optimizer.step()
 
         total_loss += loss.item()
         log_interval = 200
-        if batch % log_interval == 0 and batch > 0:
+        if i % log_interval == 0 and i > 0:
             cur_loss = total_loss / log_interval
             elapsed = time.time() - start_time
             print('| epoch {:3d} | {:5d}/{:5d} batches | '
                   'lr {:02.2f} | ms/batch {:5.2f} | '
                   'loss {:5.2f} | ppl {:8.2f}'.format(
-                    epoch, batch, len(train_data) // bptt, scheduler.get_lr()[0],
+                    epoch, i, len(train_iter), scheduler.get_lr()[0],
                     elapsed * 1000 / log_interval,
                     cur_loss, math.exp(cur_loss)))
             total_loss = 0
             start_time = time.time()
 
-def evaluate(eval_model, data_source):
+def evaluate(eval_model, data_iter):
     eval_model.eval() # Turn on the evaluation mode
     total_loss = 0.
     ntokens = len(TEXT.vocab.stoi)
     with torch.no_grad():
-        for i in range(0, data_source.size(0) - 1, bptt):
-            data, targets = get_batch(data_source, i)
-            output = eval_model(data)
-            output_flat = output.view(-1, ntokens)
-            total_loss += len(data) * criterion(output_flat, targets).item()
-    return total_loss / (len(data_source) - 1)
+        for batch in data_iter:
+            output = eval_model(batch.text)
+            loss = criterion(output.view(-1, ntokens), batch.target.view(-1)).item()
+            total_loss += loss
+    return total_loss / (len(data_iter) - 1)
 
 NO_TRAIN=False
-MODEL_PATH='./data/transformer-big.ckpt' # test loss  5.19 | test ppl   179.86
-#MODEL_PATH='./data/transformer.ckpt' # test loss  5.37 | test ppl   213.86
+#MODEL_PATH='./data/transformer-big.ckpt' # test loss  5.19 | test ppl   179.86
+MODEL_PATH='./data/transformer.ckpt' # test loss  5.37 | test ppl   213.86
 #MODEL_PATH='./data/transformer-dropout.ckpt' # test loss  5.47 | test ppl   237.30
 
 MAX_SENT_LEN=100
@@ -186,7 +169,7 @@ else:
     for epoch in range(1, epochs + 1):
         epoch_start_time = time.time()
         train()
-        val_loss = evaluate(model, val_data)
+        val_loss = evaluate(model, val_iter)
         print('-' * 89)
         print('| end of epoch {:3d} | time: {:5.2f}s | valid loss {:5.2f} | '
             'valid ppl {:8.2f}'.format(epoch, (time.time() - epoch_start_time),
@@ -203,7 +186,7 @@ else:
             sentence = ' '.join(map(lambda i: TEXT.vocab.itos[i], sample_sentence(model)))
             print(sentence)
 
-    test_loss = evaluate(best_model, test_data)
+    test_loss = evaluate(best_model, test_iter)
     print('=' * 89)
     print('| End of training | test loss {:5.2f} | test ppl {:8.2f}'.format(test_loss, math.exp(test_loss)))
     print('=' * 89)
